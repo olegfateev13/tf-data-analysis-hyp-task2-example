d2e80ea79be4d8df76bb2e653844ab1d4bee246d
olegfateev13 <58751132+olegfateev13@users.noreply.github.com>
2023-04-12 19:29:06 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ks_2samp
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 263008738 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    is_homogeneous = True
+    alpha = 0.08
+    statistic, p_value = ks_2samp(x, y)
+    if p_value < alpha:
+        is_homogeneous = False
+    return is_homogeneous
