9e3e232a3dced3965b82b2d73fb74c1a4932aae9
olegfateev13 <58751132+olegfateev13@users.noreply.github.com>
2023-04-12 21:26:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,16 @@
-import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import cramervonmises_2samp
+
+
 
 chat_id = 263008738 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    is_homogeneous = True
+    is_homogeneous = False
     alpha = 0.08
-    statistic, p_value = ks_2samp(x, y)
+    result = cramervonmises_2samp(x, y)
+    p_value = result.pvalue
+    # p_value = result.significance_level
     if p_value < alpha:
-        is_homogeneous = False
+        is_homogeneous = True
     return is_homogeneous
